39d87d09ed05942ea02fd3b8cd9c8f220ac625be
prashantiwari <tiwariprashant@outlook.com>
2018-05-14 17:24:05 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,12 +1,17 @@
 
 l = [34, "prashant", 56.78, 45, ["tiwari", 79], 56]
 
+#we can create list inside of a list, and as many nested list i want
+
 students = ["prashant", "tiwari", "sagar", "mohit", "saurav", "gaurav", "manish", "manoj"]
 
+#as we did with strings, we do all kinds of indexing / slicing / stepping
+#negetive indexing, mulitidimesional indexing
 print l[-2][1]
 
 
 print "..................................."
+
 
 n = name[0].upper()
 print n
