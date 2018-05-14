98e019e3b57fa45ac0121083151b1c4fef746fe9
prashantiwari <tiwariprashant@outlook.com>
2018-05-14 17:18:37 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,17 +1,18 @@
 a = raw_input("a : ")
 b = raw_input("b : ")
  
+#we need to change the so obtained a and b into integers for summing the values
 c = int(a) + int(b)
-
 
 print c
 
 
-if (20 < c > 100):
-	print "Greater than :20 and small than 100 "
-	print "1"
+if (c > 100:
+	print "Greater than 100"
+	if(c > 200):
+    		print "Greater than 200"
 	print "2"
 else: 
-	print "Smaller"
+	print "Smaller than 100"
 
 print "3"
