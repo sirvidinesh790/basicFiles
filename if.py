19961fdd9aa70f81029c54fb38f966506d7af104
prashantiwari <tiwariprashant@outlook.com>
2018-05-15 17:50:36 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,17 @@
+print "Hello World"
+
+a = input("Enter a : ")
+b = input("Enter b : ")
+c = a + b
+
+if(c > 100):
+	print "Greater than 100"
+	if(c > 200):
+		print "Greater than 200"
+elif(c > 50):
+	print "Sreater than 50"
+else:
+	print "Smaller than 100"
+
+
+print "End of Programe"
