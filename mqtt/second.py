a5b5164572eab3a837a304e337ea09265aab893d
prashantiwari <tiwariprashant@outlook.com>
2018-06-09 23:26:16 +0530

    Update second.py

--- mqtt/second.py
+++ mqtt/second.py
@@ -1,10 +1,8 @@
 import paho.mqtt.subscribe as subscribe
-from sys import stdout
 from time import sleep
 i = 0
 while True:
 	i = i + 1
 	msg = subscribe.simple("paho", hostname="broker.shiftr.io",auth={'password':'SomeWhereOverTheRainbows','username':'newpiforclass'})
-	stdout.write("\r"+"%s : %s" % (msg.topic, msg.payload))
-	stdout.flush()
+	print msg.payload
 	sleep(.01)
