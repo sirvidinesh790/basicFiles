9c7f2b35e23ca198b6f89d561f8b3dbee8f13e33
prashantPython <prashantonf43s@gmail.com>
2018-06-13 11:28:55 +0530

    LCD with some delet

--- mqtt/first.py
+++ mqtt/first.py
@@ -1,7 +1,12 @@
+#pip install paho-mqtt
 import paho.mqtt.publish as publish
 from time import sleep
 i = 0
 while True:
 #	sleep(1)
 	i = i + 1
-	publish.single("paho","current counter is : " + str(i), hostname="broker.shiftr.io",auth = {'username':"newpiforclass", 'password':"SomeWhereOverTheRainbows"})
+	publish.single(topic="new",
+		payload="current counter is : " + str(i),
+		hostname="broker.shiftr.io",
+		#auth = {'username':"newpiforclass", 'password':"SomeWhereOverTheRainbows"})
+		auth = {'username':"karishma","password":'qweasdzxc'})
--- mqtt/second.py
+++ mqtt/second.py
@@ -1,10 +1,13 @@
 import paho.mqtt.subscribe as subscribe
 from sys import stdout
 from time import sleep
-i = 0
 while True:
-	i = i + 1
-	msg = subscribe.simple("paho", hostname="broker.shiftr.io",auth={'password':'SomeWhereOverTheRainbows','username':'newpiforclass'})
-	stdout.write("\r"+"%s : %s" % (msg.topic, msg.payload))
+
+	msg = subscribe.simple("new",
+		hostname="broker.shiftr.io",
+		#auth={'password':'SomeWhereOverTheRainbows','username':'newpiforclass'})
+		auth={'password':'qweasdzxc',"username":"karishma"})
+#print "Message is :::: " + msg.payload
+	stdout.write("\r" +  msg.payload)
 	stdout.flush()
 	sleep(.01)
