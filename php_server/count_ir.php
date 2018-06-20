af4804bc1672754ec1f73241f0064b2ba0ede73b
prashantiwari <tiwariprashant@outlook.com>
2018-06-20 11:41:03 +0530

    Update count_ir.php

--- php_server/count_ir.php
+++ php_server/count_ir.php
@@ -1,17 +1,11 @@
 <?php
-if(isset($_GET['light'])){
-    $light = $_GET['light'];
+if(isset($_GET['count'])){
     
-    if($light == "on") {
-      $file = fopen("bulb.txt", "w") or die("can't open file");
-      fwrite($file, '1');
-      fclose($file);
-    } 
-    else if ($light == "off") {
-      $file = fopen("bulb.txt", "w") or die("can't open file");
-      fwrite($file, '0');
-      fclose($file);
-    }
+    $current_count = $_GET['count'];
+    
+    $file = fopen("count.txt", "w") or die("can't open file");
+    fwrite($file, $current_count);
+    fclose($file);
 
 }
 
@@ -22,14 +16,10 @@ if(isset($_GET['light'])){
     
     
     <title>LED for Khushbu</title>
-  
+      
   </head>
   <body>
-        <a href="google.com" >Turn On</a>
-        <br />
-
-        <a href="google.com/search/?q=rishikesh" >Turn Off</a>
-        <br />
+       <h1>See coount.txt page for counter </h1>
 
   </body>
 </html>
